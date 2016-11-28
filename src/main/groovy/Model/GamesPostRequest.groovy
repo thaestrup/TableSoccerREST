3c32f53e7ebed6f10179f6a4afdd0c0904ba649b
RobbingDaHood <Christcore1>
2016-11-28 19:18:21 +0100

    Changed input of Random tournament

--- src/main/groovy/Model/GamesPostRequest.groovy
+++ src/main/groovy/Model/GamesPostRequest.groovy
@@ -1,29 +1,18 @@
 package Model
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import groovy.sql.GroovyRowResult
 
 /**
  * Created by super on 20/11/2016.
  */
 public class GamesPostRequest {
 
-    public enum GenerationMethod {
-        RANDOM, LASTFIRST, GIVEN
-    }
-
-    private final GenerationMethod generationMethod;
     private final int numberOfGames;
-    private final List<Game> games;
     private final List<Player> players;
 
-    GamesPostRequest(@JsonProperty("generationMethod") GenerationMethod generationMethod,
-                     @JsonProperty("numberOfGames") int numberOfGames,
-                     @JsonProperty("games") List<Game> games,
+    GamesPostRequest(@JsonProperty("numberOfGames") int numberOfGames,
                      @JsonProperty("players") List<Player> players) {
-        this.generationMethod = generationMethod
         this.numberOfGames = numberOfGames
-        this.games = games
         this.players = players
     }
 
@@ -31,15 +20,7 @@ public class GamesPostRequest {
         return players
     }
 
-    GenerationMethod getGenerationMethod() {
-        return generationMethod
-    }
-
     int getNumberOfGames() {
         return numberOfGames
-    }
-
-    List<Game> getGames() {
-        return games
     }
 }
