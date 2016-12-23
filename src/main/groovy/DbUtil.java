4fc903053060667735973d98fa32223077e5c43d
= <=>
2016-12-23 09:21:00 +0100

    Fix timezone problem and make logging easier when needed

--- src/main/groovy/DbUtil.java
+++ src/main/groovy/DbUtil.java
@@ -4,33 +4,42 @@ import groovy.sql.Sql;
 import java.sql.SQLException;
 import java.util.List;
 
+import groovy.util.logging.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Created by super on 30/10/2016.
  */
 public class DbUtil {
-    public static final String url = "jdbc:mysql://localhost:3306/NykreditBF?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    public static final String url = "jdbc:mysql://localhost:3306/NykreditFoosballUnity?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=CET";
     public static final String user = "root";
     public static final String password = "";
     public static final String driver = "com.mysql.jdbc.Driver";
 
     public static String execute(String query) {
+        //Logger logger = LoggerFactory.getLogger(DbUtil.class);
         Sql sql = null;
         String resultID = "ERROR";
         try {
+            //logger.info("In try method");
             sql = Sql.newInstance(url, user, password, driver);
             sql.execute(query);
             resultID = sql.rows("SELECT LAST_INSERT_ID();").get(0).get("LAST_INSERT_ID()").toString();
             sql.close();
         } catch (ClassNotFoundException e) {
+            //logger.info("In catch for ClassNotFoundException");
             return e.toString();
         } catch (SQLException e) {
+            //logger.info("In catch for SQLException");
             return e.toString();
         } finally {
+            //logger.info("In finally");
             if (sql != null) {
                 sql.close();
             }
         }
-
         return resultID;
     }
 
@@ -54,10 +63,14 @@ public class DbUtil {
 //    }
 
     public static List<GroovyRowResult> query(String query) throws SQLException, ClassNotFoundException {
+        //Logger logger = LoggerFactory.getLogger(DbUtil.class);
+        //logger.info("In query");
         Sql sql = Sql.newInstance(url, user, password, driver);
         try {
+            //logger.info("In try in query");
             return sql.rows(query);
         } finally {
+            //logger.info("In finally in  query");
             sql.close();
         }
     }
